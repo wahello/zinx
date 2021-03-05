da23f3d44888a4ee627ccc7a850d6f275ba6fa33
zw <3362049+wahello@users.noreply.github.com>
2021-03-05 20:17:40 +0800

    Update globalobj.go

--- utils/globalobj.go
+++ utils/globalobj.go
@@ -25,7 +25,6 @@ type GlobalObj struct {
 	/*
 		Zinx
 	*/
-	Version          string //当前Zinx版本号
 	MaxPacketSize    uint32 //都需数据包的最大值
 	MaxConn          int    //当前服务器主机允许的最大链接个数
 	WorkerPoolSize   uint32 //业务工作Worker池的数量
